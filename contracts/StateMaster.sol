1b9a8868408fe78ea7ffe0ec0bb6dcee9febf7ea
HadesHappy <happyhades123@gmail.com>
2021-04-25 14:05:55 -0500

    Add StateMaster contract

---
+++ contracts/StateMaster.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import 'hardhat/console.sol';
+
+contract StateMaster {
+    constructor() // string memory _name,
+    // string memory _symbol,
+    // uint256 startingSupply
+    {
+        // _mint(msg.sender, startingSupply);
+    }
+}
