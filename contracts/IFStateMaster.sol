22c9ad84fd3934e98b4ae58b3cf4a47a084280c9
HadesHappy <happyhades123@gmail.com>
2021-04-27 14:16:32 -0500

    stake weight logic plus refactors

--- contracts/IFStateMaster.sol
+++ contracts/IFStateMaster.sol
@@ -13,19 +13,20 @@ import 'hardhat/console.sol';
 contract IFStateMaster is Ownable {
     using SafeERC20 for ERC20;
 
+    // STRUCTS
+
     // A checkpoint for marking stake amount at a given block
-    struct StakeCheckpoint {
+    struct UserCheckpoint {
         uint256 blockNumber;
         uint256 staked;
         uint128 stakeWeight;
     }
 
-    // Information of a user with respect to a specific track.
-    struct TrackUserInfo {
-        // the amount a user has staked for a track
-        uint256 staked;
-        // the number of checkpoints of a user for a track
-        uint32 numCheckpoints;
+    // A checkpoint for marking stake amount at a given block
+    struct TrackCheckpoint {
+        uint256 blockNumber;
+        uint256 totalStaked;
+        uint128 totalStakeWeight;
     }
 
     // Info of each pool.
@@ -34,6 +35,8 @@ contract IFStateMaster is Ownable {
         string name;
         // token to stake (IDIA)
         ERC20 stakeToken;
+        // weight accrual rate for this track (stake weight increase per block per stake token)
+        uint256 weightAccrualRate;
         // counts number of sales within this track
         uint256 saleCounter;
         // total staked within this track
@@ -42,21 +45,32 @@ contract IFStateMaster is Ownable {
         uint256 totalStakeWeight;
     }
 
+    // TRACK INFO
+
     // array of track information
     TrackInfo[] internal tracks;
 
-    // checkpoint mapping -- (track, user address, checkpoint number) => StakeCheckpoint
-    mapping(uint256 => mapping (address => mapping (uint32 => StakeCheckpoint))) public checkpoints;
+    // track checkpoint mapping -- (track, checkpoint number) => TrackCheckpoint
+    mapping(uint256 => mapping(uint32 => TrackCheckpoint))
+        public trackCheckpoints;
 
-    // user info mapping -- (track, user address) => user info
-    mapping(uint256 => mapping(address => TrackUserInfo))
-        public users;
+    // USER INFO
 
-    // events
+    // the number of checkpoints of a user for a track -- (track, user address) => checkpoint count
+    mapping(uint256 => mapping(address => uint32)) public userCheckpointCounts;
+
+    // user checkpoint mapping -- (track, user address, checkpoint number) => UserCheckpoint
+    mapping(uint256 => mapping(address => mapping(uint32 => UserCheckpoint)))
+        public userCheckpoints;
+
+    // EVENTS
+
     event AddTrack(string indexed name, address indexed token);
     event SetTrackUserInfo(uint256 indexed trackId, address indexed user);
 
     constructor() {}
+
+    // FUNCTIONS
 
     // number of tracks
     function trackCount() external view returns (uint256) {
@@ -64,15 +78,20 @@ contract IFStateMaster is Ownable {
     }
 
     // adds a new track
-    function addTrack(string calldata name, ERC20 stakeToken) public onlyOwner {
+    function addTrack(
+        string calldata name,
+        ERC20 stakeToken,
+        uint256 _weightAccrualRate
+    ) public onlyOwner {
         // add track
         tracks.push(
             TrackInfo({
                 name: name, // name of track
                 stakeToken: stakeToken, // token to stake (IDIA)
+                weightAccrualRate: _weightAccrualRate, // rate of stake weight accrual
                 saleCounter: 0, // default 0
                 totalStaked: 0, // default 0
-                totalStakeWeight:0 // default 0
+                totalStakeWeight: 0 // default 0
             })
         );
 
@@ -96,40 +115,44 @@ contract IFStateMaster is Ownable {
     //     return (track.stakeToken, track.saleCounter, track.totalStaked);
     // }
 
-    // sets info for a user of a particular track
-    function setTrackUserInfo(
-        uint256 trackId,
-        address user,
-        TrackUserInfo calldata userInfo
-    ) public onlyOwner {
-        // set user info
-        users[trackId][user] = userInfo;
+    // // sets info for a user of a particular track
+    // function setTrackUserInfo(
+    //     uint256 trackId,
+    //     address user,
+    //     TrackUserInfo calldata userInfo
+    // ) public onlyOwner {
+    //     // set user info
+    //     users[trackId][user] = userInfo;
 
-        // emit
-        emit SetTrackUserInfo(trackId, user);
-    }
+    //     // emit
+    //     emit SetTrackUserInfo(trackId, user);
+    // }
 
     // gets a user's stake weight within a track at a particular block number
     // logic extended from Compound COMP token `getPriorVotes` function
-    function getStakeWeight(uint256 trackId, address user, uint blockNumber) public view returns (uint256) {
-        require(blockNumber < block.number, "block # too high");
-
-        // get user info
-        TrackUserInfo storage userInfo = users[trackId][msg.sender];
+    function getStakeWeight(
+        uint256 trackId,
+        address user,
+        uint256 blockNumber
+    ) public view returns (uint256) {
+        require(blockNumber < block.number, 'block # too high');
 
         // check number of checkpoints
-        uint32 nCheckpoints = userInfo.numCheckpoints;
+        uint32 nCheckpoints = userCheckpointCounts[trackId][msg.sender];
         if (nCheckpoints == 0) {
             return 0;
         }
 
         // First check most recent checkpoint
-        if (checkpoints[trackId][user][nCheckpoints - 1].blockNumber <= blockNumber) {
-            return checkpoints[trackId][user][nCheckpoints - 1].stakeWeight;
+        if (
+            userCheckpoints[trackId][user][nCheckpoints - 1].blockNumber <=
+            blockNumber
+        ) {
+            return userCheckpoints[trackId][user][nCheckpoints - 1].stakeWeight;
         }
 
         // Next check implicit zero balance
-        if (checkpoints[trackId][user][0].blockNumber > blockNumber) {
+        if (userCheckpoints[trackId][user][0].blockNumber > blockNumber) {
             return 0;
         }
 
@@ -138,7 +161,7 @@ contract IFStateMaster is Ownable {
         uint32 upper = nCheckpoints - 1;
         while (upper > lower) {
             uint32 center = upper - (upper - lower) / 2; // ceil, avoiding overflow
-            StakeCheckpoint memory cp = checkpoints[trackId][user][center];
+            UserCheckpoint memory cp = userCheckpoints[trackId][user][center];
             if (cp.blockNumber == blockNumber) {
                 return cp.stakeWeight;
             } else if (cp.blockNumber < blockNumber) {
@@ -147,13 +170,29 @@ contract IFStateMaster is Ownable {
                 upper = center - 1;
             }
         }
-        return checkpoints[trackId][user][lower].stakeWeight;
 
+        // get closest checkpoint
+        UserCheckpoint memory closestCheckpoint =
+            userCheckpoints[trackId][user][lower];
+
+        // calculate blocks elapsed since checkpoint
+        uint256 additionalBlocks =
+            (blockNumber - closestCheckpoint.blockNumber);
+
+        // get track info
+        TrackInfo storage trackInfo = tracks[trackId];
+
+        // calculate marginal accrued stake weight
+        uint256 marginalAccruedStakeWeight =
+            additionalBlocks *
+                trackInfo.weightAccrualRate *
+                closestCheckpoint.staked;
+
+        // return
+        return closestCheckpoint.stakeWeight + marginalAccruedStakeWeight;
     }
 
     // stake
 
     // unstake
-
-
 }
