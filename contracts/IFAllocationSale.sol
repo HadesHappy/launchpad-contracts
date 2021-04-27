2451fbd532db12292d82abc400cf9e421d3a2e99
HadesHappy <happyhades123@gmail.com>
2021-04-27 14:18:33 -0500

    begin moving stake / unstake logic to state master

--- contracts/IFAllocationSale.sol
+++ contracts/IFAllocationSale.sol
@@ -72,94 +72,6 @@ contract IFAllocationSale is Ownable {
         idia = _idia;
     }
 
-    // stake
-    function stake(
-        uint256 trackId,
-        uint256 amount,
-        uint256 duration
-    ) external {
-        // stake amount must be greater than 0
-        require(amount > 0, 'amount is 0');
-        // TODO: stake amount must be <= user's balance AND limit
-
-        // user can only stake if sale is active
-        require(block.number < startBlock, 'too early');
-        require(block.number > endBlock, 'too late');
-
-        // get track info
-        SMLibrary.TrackInfo storage track = stateMaster.tracks[trackId];
-        // get user info
-        SMLibrary.UserInfo storage user =
-            stateMaster.users[trackId][msg.sender];
-
-        // TODO: Update users most recent stake time.
-
-        // transfer the specified amount of stake token from user to this contract
-        track.stakeToken.safeTransferFrom(
-            address(msg.sender),
-            address(this),
-            amount
-        );
-
-        // update tracked stake amount and stake power in user info
-        if (user.stakeAmount) {
-            user.stakeAmount = user.stakeAmount.add(amount);
-            // TODO: Add calculation logic to update stakePower
-        } else {
-            user.stakeAmount = amount;
-            user.stakePower = amount;
-        }
-
-        //
-        user.rewardDebt = user.amount.mul(track.accruedStakeAge).div(1e12);
-
-        // emit
-        emit Stake(msg.sender, trackId, amount);
-    }
-
-    // unstake
-    function unstake(
-        uint256 trackId,
-        uint256 amount,
-        uint256 duration
-    ) external {
-        // get track info
-        SMLibrary.TrackInfo storage track = stateMaster.tracks[trackId];
-        // get user info
-        SMLibrary.UserInfo storage user =
-            stateMaster.users[trackId][msg.sender];
-
-        // existing stake must be > 0 to unstake
-        require(user.stakeAmount > 0, 'no assets staked');
-        // amount to unstake must be <= stake amount
-        require(user.stakeAmount > amount, 'unstaking too much');
-
-        // duration checks
-        if (duration < 1) {
-            require(
-                block.number > user.lastDepositBlockStamp.add(1),
-                'not ready to unstake'
-            ); //Prevent Flash loan attacks?
-        } else {
-            require(
-                block.number > user.lastDepositBlockStamp.add(duration),
-                'not ready to unstake'
-            ); // Check that User's unlock time is ready
-        }
-
-        // TODO: Amount that can be claimed from the contract needs to be reduced by the amount redeemed
-        // TODO: FIX if this is the name of the totalDeposit into a Track
-        user.totalDeposit = user.totalDeposit.sub(amount);
-
-        // update user info with decreased amount
-        user.stakeAmount = user.stakeAmount.sub(amount);
-        // transfer _amount from user wallet to this contract
-        idia.safeTransfer(address(msg.sender), amount);
-
-        // emit
-        emit Unstake(msg.sender, trackId, amount, duration);
-    }
-
     // Function for the multisig to cash in the accrued idia tokens for distribution into futher baskets of usage
     // Claiming ifUSD and later needs to assign it to governance multisig to distribute to project team during fundraise
     function cash() external onlyOwner {
@@ -173,38 +85,18 @@ contract IFAllocationSale is Ownable {
         emit Cash(msg.sender, ifusdBal);
     }
 
-    // Users may call this unstake without caring about rewards. EMERGENCY ONLY.
-    // Accrued rewards are lost when this option is chosen.
-    function emergencyUnstake(uint256 _trackId) external {
-        // get user info
-        SMLibrary.UserInfo storage user =
-            stateMaster.users[_trackId][msg.sender];
-        // get user amount
-        uint256 amount = user.amount;
-
-        // reduce recorded user amount and stake power to 0
-        user.amount = 0;
-        user.stakePower = 0;
-
-        // transfer out
-        idia.safeTransfer(address(msg.sender), amount);
-
-        // emit
-        emit EmergencyUnstake(msg.sender, _trackId, amount);
-    }
-
-    // EMERGENCY ERC20 Rescue ONLY - unstake all erroneous sent in to this address.
-    // cannot unstake idia in the contract, this ensures that owner does not have a way to touch idia tokens
-    // in this contract inappropriately
-    function emergencyErc20Retrieve(address token) external onlyOwner {
-        require(token != address(idia)); // only allow retrieval for nonidia tokens
-        ERC20(token).safeTransfer(
-            address(msg.sender),
-            ERC20(token).balanceOf(address(this))
-        ); // helps remove all
-        emit EmergencyErc20Retrieve(
-            address(msg.sender),
-            ERC20(token).balanceOf(address(this))
-        );
-    }
+    // // EMERGENCY ERC20 Rescue ONLY - unstake all erroneous sent in to this address.
+    // // cannot unstake idia in the contract, this ensures that owner does not have a way to touch idia tokens
+    // // in this contract inappropriately
+    // function emergencyErc20Retrieve(address token) external onlyOwner {
+    //     require(token != address(idia)); // only allow retrieval for nonidia tokens
+    //     ERC20(token).safeTransfer(
+    //         address(msg.sender),
+    //         ERC20(token).balanceOf(address(this))
+    //     ); // helps remove all
+    //     emit EmergencyErc20Retrieve(
+    //         address(msg.sender),
+    //         ERC20(token).balanceOf(address(this))
+    //     );
+    // }
 }
--- contracts/IFStateMaster.sol
+++ contracts/IFStateMaster.sol
@@ -192,7 +192,111 @@ contract IFStateMaster is Ownable {
         return closestCheckpoint.stakeWeight + marginalAccruedStakeWeight;
     }
 
-    // stake
+    // // stake
+    // function stake(
+    //     uint256 trackId,
+    //     uint256 amount,
+    //     uint256 duration
+    // ) external {
+    //     // stake amount must be greater than 0
+    //     require(amount > 0, 'amount is 0');
+    //     // TODO: stake amount must be <= user's balance AND limit
 
-    // unstake
+    //     // user can only stake if sale is active
+    //     require(block.number < startBlock, 'too early');
+    //     require(block.number > endBlock, 'too late');
+
+    //     // get track info
+    //     SMLibrary.TrackInfo storage track = stateMaster.tracks[trackId];
+    //     // get user info
+    //     SMLibrary.UserInfo storage user =
+    //         stateMaster.users[trackId][msg.sender];
+
+    //     // TODO: Update users most recent stake time.
+
+    //     // transfer the specified amount of stake token from user to this contract
+    //     track.stakeToken.safeTransferFrom(
+    //         address(msg.sender),
+    //         address(this),
+    //         amount
+    //     );
+
+    //     // update tracked stake amount and stake power in user info
+    //     if (user.stakeAmount) {
+    //         user.stakeAmount = user.stakeAmount.add(amount);
+    //         // TODO: Add calculation logic to update stakePower
+    //     } else {
+    //         user.stakeAmount = amount;
+    //         user.stakePower = amount;
+    //     }
+
+    //     //
+    //     user.rewardDebt = user.amount.mul(track.accruedStakeAge).div(1e12);
+
+    //     // emit
+    //     emit Stake(msg.sender, trackId, amount);
+    // }
+
+    // // unstake
+    // function unstake(
+    //     uint256 trackId,
+    //     uint256 amount,
+    //     uint256 duration
+    // ) external {
+    //     // get track info
+    //     SMLibrary.TrackInfo storage track = stateMaster.tracks[trackId];
+    //     // get user info
+    //     SMLibrary.UserInfo storage user =
+    //         stateMaster.users[trackId][msg.sender];
+
+    //     // existing stake must be > 0 to unstake
+    //     require(user.stakeAmount > 0, 'no assets staked');
+    //     // amount to unstake must be <= stake amount
+    //     require(user.stakeAmount > amount, 'unstaking too much');
+
+    //     // duration checks
+    //     if (duration < 1) {
+    //         require(
+    //             block.number > user.lastDepositBlockStamp.add(1),
+    //             'not ready to unstake'
+    //         ); //Prevent Flash loan attacks?
+    //     } else {
+    //         require(
+    //             block.number > user.lastDepositBlockStamp.add(duration),
+    //             'not ready to unstake'
+    //         ); // Check that User's unlock time is ready
+    //     }
+
+    //     // TODO: Amount that can be claimed from the contract needs to be reduced by the amount redeemed
+    //     // TODO: FIX if this is the name of the totalDeposit into a Track
+    //     user.totalDeposit = user.totalDeposit.sub(amount);
+
+    //     // update user info with decreased amount
+    //     user.stakeAmount = user.stakeAmount.sub(amount);
+    //     // transfer _amount from user wallet to this contract
+    //     idia.safeTransfer(address(msg.sender), amount);
+
+    //     // emit
+    //     emit Unstake(msg.sender, trackId, amount, duration);
+    // }
+
+    // // Users may call this unstake without caring about rewards. EMERGENCY ONLY.
+    // // Accrued rewards are lost when this option is chosen.
+    // function emergencyUnstake(uint256 _trackId) external {
+    //     // get user info
+    //     SMLibrary.UserInfo storage user =
+    //         stateMaster.users[_trackId][msg.sender];
+    //     // get user amount
+    //     uint256 amount = user.amount;
+
+    //     // reduce recorded user amount and stake power to 0
+    //     user.amount = 0;
+    //     user.stakePower = 0;
+
+    //     // transfer out
+    //     idia.safeTransfer(address(msg.sender), amount);
+
+    //     // emit
+    //     emit EmergencyUnstake(msg.sender, _trackId, amount);
+    // }
 }
