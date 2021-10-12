78cb5c45eef1931218af3b0170e2169652d791f0
HadesHappy <happyhades123@gmail.com>
2021-10-12 03:26:57 -0500

    deployer script for a generic token

---
+++ scripts/deploy-GenericToken.ts
@@ -0,0 +1,31 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import hre from 'hardhat'
+
+export async function main(): Promise<void> {
+  // params
+  const name: string = process.env.NAME || ''
+  const symbol: string = process.env.SYMBOL || ''
+  const initSupply: string = process.env.INIT_SUPPLY || '' // 18 decimals assumed
+
+  // We get the contract to deploy
+  const TestTokenFactory = await hre.ethers.getContractFactory('GenericToken')
+
+  // deploy token
+  const Token = await TestTokenFactory.deploy(name, symbol, initSupply)
+
+  // log deployed addresses
+  console.log('Token deployed to ', Token.address)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error)
+    process.exit(1)
+  })
